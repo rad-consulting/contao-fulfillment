175b683c058a648826f35613e70bc014f6a4c46c
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-02-24 17:58:08 +0100

    Initial

---
+++ languages/de/tl_iso_product.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Contao extension for RAD Consulting GmbH
+ *
+ * @copyright  RAD Consulting GmbH 2016
+ * @author     Chris Raidler <c.raidler@rad-consulting.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+// Legends
+$GLOBALS['TL_LANG']['tl_iso_product']['fulfillment_legend'] = 'Distanzhandel';
+$GLOBALS['TL_LANG']['tl_iso_product']['dimension_legend'] = 'Dimensionen';
+$GLOBALS['TL_LANG']['tl_iso_product']['export_legend'] = 'Export';
+
+// Fields
+$GLOBALS['TL_LANG']['tl_iso_product']['rad_ean'] = array('EAN', 'European Article Number');
+$GLOBALS['TL_LANG']['tl_iso_product']['rad_sku'] = array('SKU', 'SKU des Produkts beim Distanzhändler.');
+$GLOBALS['TL_LANG']['tl_iso_product']['rad_stock'] = array('Lagerbestand', 'Lagerbestand des Produkts beim Distanzhändler.');
+$GLOBALS['TL_LANG']['tl_iso_product']['rad_update'] = array('Synchronisation', 'Letzte Synchronisation des Produktes mit dem Distanzhändler.');
+$GLOBALS['TL_LANG']['tl_iso_product']['rad_weight'] = array('Gewicht (Netto)', 'Netto-Gewicht des Produkts in Kilogramm.');
+$GLOBALS['TL_LANG']['tl_iso_product']['rad_width'] = array('Breite', 'Breite des Produkts in Zentimeter.');
+$GLOBALS['TL_LANG']['tl_iso_product']['rad_height'] = array('Höhe', 'Höhe des Produkts in Zentimeter.');
+$GLOBALS['TL_LANG']['tl_iso_product']['rad_length'] = array('Länge', 'Länge des Produkts in Zentimeter.');
+$GLOBALS['TL_LANG']['tl_iso_product']['rad_export'] = array('Exportieren', 'Produkt für das Exportieren an diverse Schnittstellen freigeben.');
+$GLOBALS['TL_LANG']['tl_iso_product']['rad_exported'] = array('Exportiert', 'Definiert ob das Produkt bereits exportiert wurde.');
