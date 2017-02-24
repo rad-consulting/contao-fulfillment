b6adaeb187fbd8e5ef096f3e8e19d7cd40edbc5e
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-02-25 00:28:53 +0100

    Initial

--- dca/tl_iso_product.php
+++ dca/tl_iso_product.php
@@ -95,8 +95,8 @@ $GLOBALS['TL_DCA']['tl_iso_product']['fields']['rad_weight'] = array(
     'label' => &$GLOBALS['TL_LANG']['tl_iso_product']['rad_weight'],
     'exclude' => true,
     'inputType' => 'text',
-    'eval' => array('maxlength' => 10, 'rgxp' => 'digit', 'mandatory' => true, 'tl_class' => 'w50'),
-    'attributes' => array('legend' => 'dimension_legend', 'singular' => true),
+    'eval' => array('maxlength' => 10, 'rgxp' => 'digit', 'mandatory' => true, 'tl_class' => 'w50', 'helpwizard' => true),
+    'attributes' => array('legend' => 'dimension_legend', 'singular' => true, 'type' => 'Isotope\\Model\\Attribute\\Weight'),
     'sql' => "decimal(8,2) NOT NULL default '0.00'",
 );
 
