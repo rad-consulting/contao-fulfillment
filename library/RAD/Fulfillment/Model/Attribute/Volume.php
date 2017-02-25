30b64740a7241b300ad1e589b1b082b093cec5c5
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-02-25 04:46:21 +0100

    Initial

---
+++ library/RAD/Fulfillment/Model/Attribute/Volume.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright  RAD Consulting GmbH 2017
+ * @author     Chris Raidler <c.raidler@rad-consulting.ch>
+ * @author     Olivier Dahinden <o.dahinden@rad-consulting.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+namespace RAD\Fulfillment\Model\Attribute;
+
+/**
+ * Class Volume
+ */
+class Volume extends ValueAndUnit
+{
+    /**
+     * @inheritdoc
+     */
+    const PROPERTY = 'volume';
+}
