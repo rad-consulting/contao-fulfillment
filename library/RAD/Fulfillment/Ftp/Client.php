a69718faeaa0cee934f15e1a85dedaefe65560b1
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-02-25 18:29:49 +0100

    Fulfillment

---
+++ library/RAD/Fulfillment/Ftp/Client.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ * @copyright  RAD Consulting GmbH 2017
+ * @author     Chris Raidler <c.raidler@rad-consulting.ch>
+ * @author     Olivier Dahinden <o.dahinden@rad-consulting.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+namespace RAD\Fulfillment\Ftp;
+
+use RuntimeException;
+
+/**
+ * Class Client
+ */
+class Client
+{
+    /**
+     * @var resource
+     */
+    protected $stream;
+
+    /**
+     * @var bool
+     */
+    protected $connected = false;
+
+    /**
+     * @var bool
+     */
+    protected $loggedin = false;
+
+    /**
+     * @return void
+     */
+    public function __destruct()
+    {
+        $this->close();
+    }
+
+    /**
+     * @param string $host
+     * @param int    $port
+     * @param int    $timeout
+     * @return $this
+     * @throws RuntimeException
+     */
+    public function connect($host, $port = 21, $timeout = 90)
+    {
+        $stream = ftp_connect($host, $port, $timeout);
+
+        if (!is_resource($stream)) {
+            throw new RuntimeException("Failed to connect to '{$host}:{$port}'");
+        }
+
+        $this->stream = $stream;
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function close()
+    {
+        if (is_resource($this->stream)) {
+            ftp_close($this->stream);
+            $this->stream = null;
+            $this->connected = false;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string $remote
+     * @return $this
+     * @throws RuntimeException
+     */
+    public function deleteFile($remote)
+    {
+        if ($this->isLoggedIn()) {
+            if (!ftp_delete($this->stream, $remote)) {
+                throw new RuntimeException("Failed to delete file '{$remote}'");
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string $local
+     * @param string $remote
+     * @param int    $mode
+     * @return $this
+     * @throws RuntimeException
+     */
+    public function downloadFile($local, $remote, $mode = FTP_BINARY)
+    {
+        if ($this->isLoggedIn()) {
+            if (false === ftp_get($this->stream, $local, $remote, $mode)) {
+                throw new RuntimeException("Failed to download file '{$remote}' to '{$local}'");
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param $username
+     * @param $password
+     * @return $this
+     * @throws RuntimeException
+     */
+    public function login($username, $password)
+    {
+        if (!ftp_login($this->stream, $username, $password)) {
+            throw new RuntimeException('Failed to login with username/password');
+        }
+
+        $this->loggedin = true;
+
+        return $this;
+    }
+
+    /**
+     * @param string $remote
+     * @param string $local
+     * @param int    $mode
+     * @return $this
+     * @throws RuntimeException
+     */
+    public function uploadFile($remote, $local, $mode = FTP_BINARY)
+    {
+        if ($this->isLoggedIn()) {
+            if (false === ftp_put($this->stream, $remote, $local, $mode)) {
+                throw new RuntimeException("Failed to upload file '{$local}' to '{$remote}'");
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param bool $passive
+     * @return $this
+     * @throws RuntimeException
+     */
+    public function setPassive($passive = true)
+    {
+        if ($this->isConnected()) {
+            ftp_pasv($this->stream, $passive);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param $filename
+     * @return bool
+     * @throws RuntimeException
+     */
+    public function getSize($filename)
+    {
+        if ($this->isLoggedIn()) {
+            return (bool)ftp_size($this->stream, $filename);
+        }
+
+        return false;
+    }
+
+    /**
+     * @return bool
+     * @throws RuntimeException
+     */
+    public function isConnected()
+    {
+        if (is_resource($this->stream) && $this->connected) {
+            return true;
+        }
+
+        throw new RuntimeException('Not connected');
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLoggedIn()
+    {
+        return $this->isConnected() && $this->loggedin;
+    }
+}
