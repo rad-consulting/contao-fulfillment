c80707acd9fbd5bb1b1adf9bce535b4bfc27d060
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-02-25 14:53:01 +0100

    Fulfillment

--- languages/de/tl_rad_fulfillment.php
+++ languages/de/tl_rad_fulfillment.php
@@ -7,3 +7,13 @@
  */
 
 // Legends
+$GLOBALS['TL_LANG']['tl_rad_fulfillment']['type_legend'] = 'Warenausgang';
+$GLOBALS['TL_LANG']['tl_rad_fulfillment']['fulfillment_legend'] = 'Kundenbestellung';
+
+// Fields
+$GLOBALS['TL_LANG']['tl_rad_fulfillment']['id'] = array('ID');
+$GLOBALS['TL_LANG']['tl_rad_fulfillment']['pid'] = array('Bestellung');
+$GLOBALS['TL_LANG']['tl_rad_fulfillment']['producttype'] = array('Produkt-Typ');
+$GLOBALS['TL_LANG']['tl_rad_fulfillment']['status'] = array('Status');
+$GLOBALS['TL_LANG']['tl_rad_fulfillment']['reference'] = array('Referenz', 'Referenz der Kundenbestellung beim Distributeur.');
+$GLOBALS['TL_LANG']['tl_rad_fulfillment']['tracking'] = array('Tracking-Nummern', 'Informationen zur Nachverfolgung der Sendung.');
