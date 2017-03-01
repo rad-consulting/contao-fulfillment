3682547422452012550978da232e071b147c6218
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-03-01 12:15:39 +0100

    Fulfillment

--- languages/de/tl_rad_supplier_order.php
+++ languages/de/tl_rad_supplier_order.php
@@ -17,12 +17,12 @@ $GLOBALS['TL_LANG']['tl_rad_supplier_order']['export_legend'] = 'Export';
 
 // Fields
 $GLOBALS['TL_LANG']['tl_rad_supplier_order']['id'] = array('ID');
-$GLOBALS['TL_LANG']['tl_rad_supplier_order']['name'] = array('Name');
-$GLOBALS['TL_LANG']['tl_rad_supplier_order']['producttype'] = array('Produkte-Typ');
+$GLOBALS['TL_LANG']['tl_rad_supplier_order']['name'] = array('Name', 'Interner Name der Bestellung.');
+$GLOBALS['TL_LANG']['tl_rad_supplier_order']['producttype'] = array('Produkte-Typ', 'Wählen Sie den Produkte-Typ.');
 $GLOBALS['TL_LANG']['tl_rad_supplier_order']['tstamp'] = array('Erstellungsdatum');
-$GLOBALS['TL_LANG']['tl_rad_supplier_order']['positions'] = array('Positionen');
+$GLOBALS['TL_LANG']['tl_rad_supplier_order']['positions'] = array('Positionen', 'Wählen Sie die gewünschten Produkte und geben Sie deren zu bestellende Anzahl ein.');
 $GLOBALS['TL_LANG']['tl_rad_supplier_order']['product'] = array('Produkt');
 $GLOBALS['TL_LANG']['tl_rad_supplier_order']['quantity'] = array('Anzahl');
-$GLOBALS['TL_LANG']['tl_rad_supplier_order']['export'] = array('Exportieren');
-$GLOBALS['TL_LANG']['tl_rad_supplier_order']['exported'] = array('Bereits exportiert');
-$GLOBALS['TL_LANG']['tl_rad_supplier_order']['reference'] = array('Referenz');
+$GLOBALS['TL_LANG']['tl_rad_supplier_order']['export'] = array('Exportieren', 'Markieren wenn die Bestellung an ein externes System übermittelt werden soll.');
+$GLOBALS['TL_LANG']['tl_rad_supplier_order']['exported'] = array('Bereits exportiert', 'Zeigt ob die Bestellung bereits an ein externes System übermittelt wurde.');
+$GLOBALS['TL_LANG']['tl_rad_supplier_order']['reference'] = array('Referenz', 'Die Referenz aus dem externen System wird, falls vorhanden, hier vermerkt.');
