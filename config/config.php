d18b2e8b26e452231a8f742d48da29d441be8f15
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-03-01 13:18:43 +0100

    Fulfillment

--- config/config.php
+++ config/config.php
@@ -6,15 +6,7 @@
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
 
-// Models
-\Isotope\Model\Shipping::registerModelType('swisspost', 'RAD\\Fulfillment\\Model\\Shipping\\Swisspost');
-$GLOBALS['TL_MODELS'][\RAD\Fulfillment\Model\Fulfillment::getTable()] = 'RAD\\Fulfillment\\Model\\Fulfillment';
-$GLOBALS['TL_MODELS'][\RAD\Fulfillment\Model\SupplierOrder::getTable()] = 'RAD\\Fulfillment\\Model\\SupplierOrder';
-
-$GLOBALS['ISO_HOOKS']['postCheckout'][] = array('RAD\\Fulfillment\\Service', 'onPostCheckout');
-$GLOBALS['RAD_SUBSCRIBERS'][] = 'RAD\\Fulfillment\\Service';
-$GLOBALS['BE_MOD']['isotope']['iso_products']['tables'][] = 'tl_rad_log';
-
+// Backend modules
 if (!is_array($GLOBALS['BE_MOD']['fulfillment'])) {
     $index = array_search('isotope', array_keys($GLOBALS['BE_MOD']));
     array_insert($GLOBALS['BE_MOD'], $index + 1, array('fulfillment' => array(
@@ -29,16 +21,18 @@ if (!is_array($GLOBALS['BE_MOD']['fulfillment'])) {
     )));
 }
 
-$GLOBALS['RAD_LOG_ENTITIES']['fulfillments'] = array(
-    'ptable' => 'tl_rad_fulfillment',
-);
+// Models
+\Isotope\Model\Shipping::registerModelType('swisspost', 'RAD\\Fulfillment\\Model\\Shipping\\Swisspost');
+$GLOBALS['TL_MODELS'][\RAD\Fulfillment\Model\Fulfillment::getTable()] = 'RAD\\Fulfillment\\Model\\Fulfillment';
+$GLOBALS['TL_MODELS'][\RAD\Fulfillment\Model\SupplierOrder::getTable()] = 'RAD\\Fulfillment\\Model\\SupplierOrder';
 
-$GLOBALS['RAD_LOG_ENTITIES']['supplierorders'] = array(
-    'ptable' => 'tl_rad_supplier_order',
-    'headerFields' => array('id', 'name', 'tstamp'),
-);
+// Isotope hooks
+$GLOBALS['ISO_HOOKS']['generateProduct'][] = array('RAD\\Fulfillment\\Frontend', 'onGenerateProduct');
+$GLOBALS['ISO_HOOKS']['postCheckout'][] = array('RAD\\Fulfillment\\Service', 'onPostCheckout');
 
-$GLOBALS['RAD_LOG_ENTITIES']['iso_products'] = array(
-    'ptable' => 'tl_iso_product',
-    'headerFields' => array('id', 'name', 'tstamp'),
-);
+// Modifications
+$GLOBALS['BE_MOD']['isotope']['iso_products']['tables'][] = 'tl_rad_log';
+$GLOBALS['RAD_SUBSCRIBERS'][] = 'RAD\\Fulfillment\\Service';
+$GLOBALS['RAD_LOG_ENTITIES']['fulfillments'] = array('ptable' => 'tl_rad_fulfillment');
+$GLOBALS['RAD_LOG_ENTITIES']['supplierorders'] = array('ptable' => 'tl_rad_supplier_order', 'headerFields' => array('id', 'name', 'tstamp'));
+$GLOBALS['RAD_LOG_ENTITIES']['iso_products'] = array('ptable' => 'tl_iso_product', 'headerFields' => array('id', 'name', 'tstamp'));
---
+++ library/RAD/Fulfillment/Frontend.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @copyright  RAD Consulting GmbH 2017
+ * @author     Chris Raidler <c.raidler@rad-consulting.ch>
+ * @author     Olivier Dahinden <o.dahinden@rad-consulting.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+namespace RAD\Fulfillment\Frontend;
+
+use Contao\Frontend as ContaoFrontend;
+use Isotope\Model\Product;
+use Isotope\Template;
+use RAD\Fulfillment\Model\Product\Fulfillment;
+
+/**
+ * Class Frontend
+ */
+class Frontend extends ContaoFrontend
+{
+    /**
+     * @param Template $template
+     * @param Product  $product
+     * @return void
+     */
+    public function onGenerateProduct(Template $template, Product $product)
+    {
+        $template->isAvailable = function () use ($product) {
+            if ($product instanceof Fulfillment) {
+                return (bool)$product->getStock();
+            }
+
+            return true;
+        };
+
+        $template->getStock = function () use ($product) {
+            if ($product instanceof Fulfillment) {
+                return $product->getStock();
+            }
+
+            return 0;
+        };
+
+        $template->hasStock = function () use ($product) {
+            if ($product instanceof Fulfillment) {
+                return (bool)$product->getStock();
+            }
+
+            return true;
+        };
+    }
+}
