cca9f432eacec70a312e19a2e6aad82ab4314f0a
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-03-01 12:16:20 +0100

    Fulfillment

--- languages/de/tl_rad_supplier_order.php
+++ languages/de/tl_rad_supplier_order.php
@@ -23,6 +23,6 @@ $GLOBALS['TL_LANG']['tl_rad_supplier_order']['tstamp'] = array('Erstellungsdatum
 $GLOBALS['TL_LANG']['tl_rad_supplier_order']['positions'] = array('Positionen', 'Wählen Sie die gewünschten Produkte und geben Sie deren zu bestellende Anzahl ein.');
 $GLOBALS['TL_LANG']['tl_rad_supplier_order']['product'] = array('Produkt');
 $GLOBALS['TL_LANG']['tl_rad_supplier_order']['quantity'] = array('Anzahl');
-$GLOBALS['TL_LANG']['tl_rad_supplier_order']['export'] = array('Exportieren', 'Markieren wenn die Bestellung an ein externes System übermittelt werden soll.');
+$GLOBALS['TL_LANG']['tl_rad_supplier_order']['export'] = array('Exportieren', 'Markieren, sofern die Bestellung an ein externes System übermittelt werden soll.');
 $GLOBALS['TL_LANG']['tl_rad_supplier_order']['exported'] = array('Bereits exportiert', 'Zeigt ob die Bestellung bereits an ein externes System übermittelt wurde.');
 $GLOBALS['TL_LANG']['tl_rad_supplier_order']['reference'] = array('Referenz', 'Die Referenz aus dem externen System wird, falls vorhanden, hier vermerkt.');
