a1e79bd2debc4147555735e2cd18807f76adcf0a
Christof W. Raidler <c.raidler@rad-consulting.ch>
2017-03-05 11:00:04 +0100

    Fulfillment

--- library/RAD/Fulfillment/Backend/Panel.php
+++ library/RAD/Fulfillment/Backend/Panel.php
@@ -137,7 +137,7 @@ class Panel extends Backend
      */
     public function buttonsForFulfillment(array &$buttons)
     {
-        array_unshift($buttons, '<fieldset id="pal_position_legend" class="tl_box"><legend onclick="AjaxRequest.toggleFieldset(this,\'fulfillment_legend\',\'tl_rad_fulfillment\')">Positionen</legend><div>hallo</div></fieldset>');
+        array_unshift($buttons, '<fieldset id="pal_position_legend" class="tl_box"><legend onclick="AjaxRequest.toggleFieldset(this,\'position_legend\',\'tl_rad_fulfillment\')">Positionen</legend><div>hallo</div></fieldset>');
 
         return $buttons;
     }
